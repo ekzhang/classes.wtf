0e2bde3b3c806a07068cb8f8d0c3082187f43396
Eric Zhang <ekzhang1@gmail.com>
2022-08-08 01:44:10 -0400

    Add instructors list to the catalog

--- frontend/src/lib/Course.svelte
+++ frontend/src/lib/Course.svelte
@@ -8,8 +8,18 @@
   <h3 class="text-sm font-bold">
     {data.subject}
     {data.catalogNumber}
-    {data.title} ({data.semester})
+    {data.title ?? "[No Title]"} ({data.semester})
   </h3>
+  <p class="text-sm mb-1">
+    {#each data.courseInstructors as instructor, i}
+      <a href={instructor.email && `mailto:${instructor.email.toLowerCase()}`}
+        >{instructor.displayName}</a
+      >{#if i < data.courseInstructors.length - 1}{", "}{/if}
+    {/each}
+  </p>
+  <p class="text-xs font-light mb-1">
+    {data.academicGroup} | {data.courseLevel} | {data.componentFiltered}
+  </p>
   <div class="text-xs">
     {@html data.courseDescriptionLong
       .replaceAll("&nbsp;", "\xa0")
